cf15e036df41fb59505d199406cc9f6e9d466791
Wiktor Dziendziel <wiktordablju@gmail.com>
2024-11-08 14:19:59 +0100

    Update zad01.py

--- zad01.py
+++ zad01.py
@@ -1,9 +1,9 @@
 import turtle
-t = turtle.Turtle()
-t.speed(0)
+t = turtle.Turtle('turtle')
+# t.speed(0)
 window = turtle.Screen()
 
-
+t.left(60)
 def zad01(bok, min):
     if (bok < min):
         return
@@ -12,7 +12,7 @@ def zad01(bok, min):
         i += 1
         zad01(bok/3, min)
         t.forward(bok)
-        t.left(60)
+        t.right(60)
 
 
 zad01(150, 50)
