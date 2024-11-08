3fddcac6702a70c3c36de428dff8f215b286e71c
Wiktor Dziendziel <wiktordablju@gmail.com>
2024-11-08 14:20:10 +0100

    Update zad02.py

--- zad02.py
+++ zad02.py
@@ -1,24 +1,26 @@
 import turtle
-t = turtle.Turtle()
-t.speed(0)
+t = turtle.Turtle('turtle')
+# t.speed(0)
 window = turtle.Screen()
 
+t.left(90)
 
 def zad02(bok, min):
     if (bok < min):
         return
-    i = 0
-    while i < 2:
-        i += 1
-        t.forward(bok)
-        t.left(90)
-        t.forward(bok*3/4)
-        t.left(180)
-        zad02(bok/2, min)
-        t.left(180)
+    for i in range (2):
+        # BOK LEWO-PRAWO
         t.forward(bok/4)
         t.left(90)
+        zad02(bok/2, min)  
+        t.right(90)
+        t.forward(3*bok/4)
+
+        # PODSTAWA
+        t.right(90)
+        t.forward(bok)
+        t.right(90)
 
 
-zad02(200, 100)
+zad02(200, 200)
 window.exitonclick()
